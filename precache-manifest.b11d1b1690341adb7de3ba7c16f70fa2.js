1a1d263b28bb443b6d3a0a665df0c610ba0fe396
roberts98 <robertsidzinka@interia.pl>
2020-02-21 23:43:06 +0100

    Updates

---
+++ precache-manifest.b11d1b1690341adb7de3ba7c16f70fa2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9d63b6c85a0a2856aee137727d3a3ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c871df0dd7400c0144c8",
+    "url": "/static/js/2.fcd49701.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.fcd49701.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fa1d2186cf03f5b86b8",
+    "url": "/static/js/main.8a47367b.chunk.js"
+  },
+  {
+    "revision": "723b3cc9b8ec153a2e23",
+    "url": "/static/js/runtime-main.d4991002.js"
+  }
+]);
