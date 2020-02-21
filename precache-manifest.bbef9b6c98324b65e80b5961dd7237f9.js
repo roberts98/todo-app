3cffdf5a6b91479d37911a0a9cee748e42f5df23
roberts98 <robertsidzinka@interia.pl>
2020-02-21 23:52:32 +0100

    Updates

---
+++ precache-manifest.bbef9b6c98324b65e80b5961dd7237f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d620c293e1396ed3515526fcc74f9ce",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "42214bdc1fa24ba47b3d",
+    "url": "/todo-app/static/js/2.e01e9e5e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/todo-app/static/js/2.e01e9e5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1c6d97de4b114810e9a",
+    "url": "/todo-app/static/js/main.ac4ea48e.chunk.js"
+  },
+  {
+    "revision": "b66ecab6fc712c6d22a7",
+    "url": "/todo-app/static/js/runtime-main.e77e13dd.js"
+  },
+  {
+    "revision": "facd84e912166e759ec9526c2df3f6d0",
+    "url": "/todo-app/static/media/checked.facd84e9.svg"
+  },
+  {
+    "revision": "223a3e98217f080709225ae8621a7333",
+    "url": "/todo-app/static/media/plus.223a3e98.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b11d1b1690341adb7de3ba7c16f70fa2.js"
+  "/todo-app/precache-manifest.bbef9b6c98324b65e80b5961dd7237f9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todo-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
